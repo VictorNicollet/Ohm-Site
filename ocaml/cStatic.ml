9cdfbb2a66002352a5a7b779335c59d74999b452
Victor Nicollet <victor@nicollet.net>
2012-09-06 18:26:19 +0200

    Use static page rendering

--- ocaml/cStatic.ml
+++ ocaml/cStatic.ml
@@ -1,3 +1,11 @@
 (* Ohm is © 2012 Victor Nicollet *) 
 
-let () = OhmStatic.export ~server:O.server ~title:"Ohm Framework" Static.site
+open Ohm
+open Ohm.Universal
+open BatPervasives
+
+let render _ ?css ?js ?head ?favicon ?body_classes ~title writer =
+  let! writer = ohm $ Asset_Static_Page.render writer in
+  return $ O.page ?css ?js ?head ?favicon ?body_classes ~title writer
+
+let () = OhmStatic.export ~render ~server:O.server ~title:"Ohm Framework" Static.site
