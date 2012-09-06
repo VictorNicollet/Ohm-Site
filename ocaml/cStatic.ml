fdbecf8c22ce34040c8fe60439d8ed440288d8a6
Victor Nicollet <victor@nicollet.net>
2012-09-06 19:23:42 +0200

    Allow translations

--- ocaml/cStatic.ml
+++ ocaml/cStatic.ml
@@ -5,7 +5,9 @@ open Ohm.Universal
 open BatPervasives
 
 let render _ ?css ?js ?head ?favicon ?body_classes ~title writer =
-  let! writer = ohm $ Asset_Static_Page.render writer in
-  return $ O.page ?css ?js ?head ?favicon ?body_classes ~title writer
+  Run.with_context (O.ctx `EN) begin 
+    let! writer = ohm $ Asset_Static_Page.render writer in
+    return $ O.page ?css ?js ?head ?favicon ?body_classes ~title writer
+  end
 
 let () = OhmStatic.export ~render ~server:O.server ~title:"Ohm Framework" Static.site
