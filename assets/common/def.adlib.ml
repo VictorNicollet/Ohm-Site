4e212048ad7ce637b9036fed6e64a57e592792ba
Victor Nicollet <victor@nicollet.net>
2012-09-25 11:21:59 +0200

    Title bar

--- assets/common/def.adlib.ml
+++ assets/common/def.adlib.ml
@@ -1,1 +1,5 @@
 | `Common_Project_Name
+
+| `Common_Title_Tutorials 
+| `Common_Title_Download 
+| `Common_Title_Documentation 
--- assets/common/en.adlib.ml
+++ assets/common/en.adlib.ml
@@ -1,1 +1,5 @@
 | `Common_Project_Name -> ConfigProject.name 
+
+| `Common_Title_Tutorials -> "Tutorials"
+| `Common_Title_Download -> "Download"
+| `Common_Title_Documentation -> "Documentation"
