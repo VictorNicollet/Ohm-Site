581de465685ed55578dd31f87554f2059a61a4d0
Victor Nicollet <victor@nicollet.net>
2012-09-25 11:44:01 +0200

    Current section display

--- ocaml/cStatic.ml
+++ ocaml/cStatic.ml
@@ -4,13 +4,25 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
+type sections = [`Download|`Tutorials|`Docs|`Other]
+
 let render = 
 
-  let page body = Asset_Static_Page.render (object
+  let page (section:sections) body = Asset_Static_Page.render (object
     method body = body
-    method head = ()
+    method head = object
+      method download  = section = `Download
+      method tutorials = section = `Tutorials 
+      method docs      = section = `Docs
+    end
   end) in
 
-  OhmStatic.wrap page |> OhmStatic.with_context (O.ctx `EN)
+  let render section = OhmStatic.with_context O.ctx `EN (OhmStatic.wrap (page section)) in
+
+  OhmStatic.prefixed_render
+    ~default:(render `Other)
+    [ "tutorials/" , render `Tutorials ;
+      "download/"  , render `Download  ;
+      "docs/"      , render `Docs      ]
 
 let () = OhmStatic.export ~render ~server:O.server ~title:"Ohm Framework" Static.site
