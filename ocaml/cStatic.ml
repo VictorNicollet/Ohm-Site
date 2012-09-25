ffb097f67b92d7180b68f9224bd48ffcffd5c3ac
Victor Nicollet <victor@nicollet.net>
2012-09-25 09:40:13 +0200

    Adjust to new ohmStatic module interface

--- ocaml/cStatic.ml
+++ ocaml/cStatic.ml
@@ -4,10 +4,7 @@ open Ohm
 open Ohm.Universal
 open BatPervasives
 
-let render _ ?css ?js ?head ?favicon ?body_classes ~title writer =
-  Run.with_context (O.ctx `EN) begin 
-    let! writer = ohm $ Asset_Static_Page.render writer in
-    return $ O.page ?css ?js ?head ?favicon ?body_classes ~title writer
-  end
+let render = 
+  OhmStatic.wrap Asset_Static_Page.render |> OhmStatic.with_context (O.ctx `EN)
 
 let () = OhmStatic.export ~render ~server:O.server ~title:"Ohm Framework" Static.site
